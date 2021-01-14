7c6ea95cc4fe1f55e129a58e38383913835711a8
aboubakar ismael <aboubakarismael16@gmail.com>
2021-01-14 16:02:54 +0800

    second commit

--- LeetCode_Binary_Tree/problem98.go
+++ LeetCode_Binary_Tree/problem98.go
@@ -2,18 +2,17 @@ package LeetCode_Binary_Tree
 
 import "math"
 
-func isValidBST(root *TreeNode) bool {
+
+func isValidBST(root *TreeNode) bool  {
 	return isValidBSTHelper(root,math.MinInt32,math.MaxInt32)
+
 }
 
-func isValidBSTHelper(root *TreeNode,min,max int) bool  {
+func isValidBSTHelper(root *TreeNode,min,max int) bool {
 	if root == nil {
 		return true
 	}
-	if root.Val < min || root.Val > max {
-		return false
-	}
 
-	return isValidBSTHelper(root.Left,min,root.Val - 1) && isValidBSTHelper(root.Right,max,root.Val +1)
-	
+	return isValidBSTHelper(root.Left,min,root.Val-1) &&
+	       isValidBSTHelper(root.Right,root.Val,max)
 }
