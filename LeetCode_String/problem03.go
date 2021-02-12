cf6c952cad4fd4a4afb24e886c8094287924d047
aboubakar ismael <aboubakarismael16@gmail.com>
2021-02-12 12:49:40 +0800

    second commit

---
+++ LeetCode_String/problem03.go
@@ -0,0 +1,28 @@
+package LeetCode_String
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	var bitSet [256]bool
+	result,left,right := 0,0,0
+
+	for left < len(s) {
+		if bitSet[s[right]] {
+			bitSet[s[left]] = false
+			left++
+		} else {
+			bitSet[s[right]] = true
+			right++
+		}
+		if result < right-left {
+			result = right-left
+		}
+		if left+result >= len(s) || right >= len(s) {
+			break
+		}
+	}
+
+	return result
+}
