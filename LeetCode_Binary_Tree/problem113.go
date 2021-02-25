25c7aa742eec919579649d46e824bfeca4eedf8c
aboubakar ismael <aboubakarismael16@gmail.com>
2021-02-25 11:48:52 +0800

    second commit

---
+++ LeetCode_Binary_Tree/problem113.go
@@ -0,0 +1,25 @@
+package LeetCode_Binary_Tree
+
+func pathSum(root *TreeNode, sum int) [][]int {
+    var slice [][]int
+    slice = findPath(root, sum, slice, []int(nil))
+    
+    return slice
+}
+
+func findPath(n *TreeNode, sum int, slice [][]int, stack []int) [][]int {
+    if n == nil {
+        return slice
+    }
+    
+    sum -= n.Val
+    stack = append(stack, n.Val)
+    if sum == 0 && n.Left == nil && n.Right == nil {
+        slice = append(slice, append([]int{}, stack...))
+        stack = stack[:len(stack)-1]
+    }
+    slice = findPath(n.Left, sum, slice, stack)
+    slice = findPath(n.Right, sum, slice, stack)
+    
+    return slice
+}
