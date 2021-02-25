7cd8e837065722b6c66e02396a37bb49b605d978
aboubakar ismael <aboubakarismael16@gmail.com>
2021-02-25 12:43:51 +0800

    second commit

---
+++ LeetCode_Binary_Tree/problem124.go
@@ -0,0 +1,36 @@
+package LeetCode_Binary_Tree
+
+import "math"
+
+func maxPathSum(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    
+    max := math.MinInt32
+    getPathSum(root, &max)
+    
+    return max
+}
+
+func getPathSum(root *TreeNode, maxSum *int) int {
+    if root == nil {
+        return math.MinInt32
+    }
+    
+    left := getPathSum(root.Left, maxSum)
+    right := getPathSum(root.Right, maxSum)
+    
+    currMax := max(max(left+root.Val, right+root.Val), root.Val)
+    *maxSum = max(*maxSum, max(currMax, left+right+root.Val))
+    
+    return currMax
+}
+
+func max(a,b int) int {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
