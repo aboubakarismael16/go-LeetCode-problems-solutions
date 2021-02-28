6545a866bc3fff86cf6243b5b5f3c95a57ea56c0
aboubakar ismael <aboubakarismael16@gmail.com>
2021-02-28 08:54:31 +0800

    second commit

---
+++ problem1143.go
@@ -0,0 +1,28 @@
+package LeetCode
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+    l1,l2 := len(text1), len(text2)
+    dp := make([][]int, l1 + 1)
+    for i := range dp {
+        dp[i] = make([]int, l2 + 1)
+    }
+    for i := 1; i <= l1; i++ {
+        for j := 1; j <= l2; j++  {
+            if text1[i-1] == text2[j-1] {
+                 dp[i][j] = dp[i-1][j-1] + 1
+            } else {
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+
+    return dp[l1][l2]
+}
+
+func max(a,b int) int {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
