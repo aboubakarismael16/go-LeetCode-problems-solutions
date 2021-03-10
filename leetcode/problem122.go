b3d9d3a15b41df34bbeee0a1c6af73b6d78315fb
aboubakar ismael <aboubakarismael16@gmail.com>
2021-03-11 07:31:27 +0800

    second commit

---
+++ leetcode/problem122.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func maxProfit(prices []int) int {
+	if prices == nil || len(prices) == 0 {
+		return 0
+	}
+
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
+		}
+	}
+
+	return maxProfit
+}
