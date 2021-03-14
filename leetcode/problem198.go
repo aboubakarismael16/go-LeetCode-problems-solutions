373fedc67bae30951e677f35d4a8374a837c627a
aboubakar ismael <aboubakarismael16@gmail.com>
2021-03-14 09:58:35 +0800

    second commit

---
+++ leetcode/problem198.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func rob(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return nums[0]
+	}
+	dp := make([]int, n)
+	dp[0], dp[1] = nums[0], max(nums[1], nums[0])
+	for i := 2; i < n; i++ {
+		dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+	}
+
+	return dp[n-1]
+}
