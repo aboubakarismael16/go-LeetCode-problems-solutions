1b9b6a7ec450ed42867d1c771618e447b51e678b
aboubakar ismael <aboubakarismael16@gmail.com>
2021-05-01 10:46:51 +0800

    second commit

--- LeetCode_Linked_List/problem206.go
+++ LeetCode_Linked_List/problem206.go
@@ -1,14 +1,25 @@
 package LeetCode_Linked_List
 
-
 func reverseList(head *ListNode) *ListNode {
 	var behind *ListNode
 	for head != nil {
 		next := head.Next
-		head.Next = behind 
-		behind = head 
-		head = next 
+		head.Next = behind
+		behind = head
+		head = next
 	}
 
 	return behind
+}
+
+// second method 
+## just more easy than the frist one
+
+func reverseList2(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		head.Next, prev, head = prev, head, head.Next
+	}
+
+	return prev
 }
