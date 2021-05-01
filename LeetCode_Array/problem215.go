b5e4c1306503bf1eda0951bf2915d5d346d6a63f
aboubakar ismael <aboubakarismael16@gmail.com>
2021-05-01 11:48:41 +0800

    second commit

--- LeetCode_Array/problem215.go
+++ LeetCode_Array/problem215.go
@@ -9,39 +9,49 @@ func findKthLargest(nums []int, k int) int {
 }
 
 //second method
-func findKthLargest(nums []int, k int) int {
-    if len(nums) == 0 {
-        return 0
-    }
+func findKthLargest2(nums []int, k int) int {
+	if len(nums) == 0 {
+		return 0
+	}
 
-    return selection(nums, 0, len(nums)-1, len(nums)-k)
+	return selection(nums, 0, len(nums)-1, len(nums)-k)
 }
 
-func selection(nums []int, l,r,k int) int {
-    if l == r {
-        return nums[l]
-    }
+func selection(nums []int, l, r, k int) int {
+	if l == r {
+		return nums[l]
+	}
 
-    p := partition(nums, l, r)
-    if k == p {
-        return nums[p]
-    } else if k < p {
-        return selection(nums, l, p-1, k)
-    } else {
-        return selection(nums, p+1, r, k)
-    }
+	p := partition(nums, l, r)
+	if k == p {
+		return nums[p]
+	} else if k < p {
+		return selection(nums, l, p-1, k)
+	} else {
+		return selection(nums, p+1, r, k)
+	}
 }
 
-func partition(nums []int, low,high int) int {
-    pivot := nums[high]
-    i := low - 1
-    for j := low; j < high; j++ {
-        if nums[j] < pivot {
-            i++
-            nums[j], nums[i] = nums[i], nums[j]
-        }
-    }
-    nums[i+1], nums[high] = nums[high], nums[i+1]
+func partition(nums []int, low, high int) int {
+	pivot := nums[high]
+	i := low - 1
+	for j := low; j < high; j++ {
+		if nums[j] < pivot {
+			i++
+			nums[j], nums[i] = nums[i], nums[j]
+		}
+	}
+	nums[i+1], nums[high] = nums[high], nums[i+1]
 
-    return i + 1
+	return i + 1
+}
+
+//Third method
+
+func findKthLargest3(nums []int, k int) int {
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] > nums[j]
+	})
+
+	return nums[k-1]
 }
