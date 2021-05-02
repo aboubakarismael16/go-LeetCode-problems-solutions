e28d60df8f00e79927847ef2db3bf336c321eed2
aboubakar ismael <aboubakarismael16@gmail.com>
2021-05-02 22:58:06 +0800

    second commit

--- LeetCode_Binary_Tree/problem104.go
+++ LeetCode_Binary_Tree/problem104.go
@@ -1,22 +1,37 @@
 package LeetCode_Binary_Tree
 
 func maxDepth(root *TreeNode) int {
-    depth := 0
-    return findMax(root,depth)
+	depth := 0
+	return findMax(root, depth)
 }
 
-func findMax(root *TreeNode,depth int) int {
-    if root == nil {
-        return depth
-    }
-    
-    return max(findMax(root.Left,depth + 1),findMax(root.Right,depth + 1))
+func findMax(root *TreeNode, depth int) int {
+	if root == nil {
+		return depth
+	}
+
+	return max(findMax(root.Left, depth+1), findMax(root.Right, depth+1))
 }
 
-func max(a,b int) int {
-    if a > b {
-        return a
-    } else {
-        return b
-    }
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// second method
+func maxDepth2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	} else {
+		return rightDepth + 1
+	}
 }
