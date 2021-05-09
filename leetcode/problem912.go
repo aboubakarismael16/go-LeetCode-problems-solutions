4a394a50531dd41e38b3f2a0cf0a15e0c0508371
aboubakar ismael <aboubakarismael16@gmail.com>
2021-05-09 22:47:45 +0800

    second commit

--- leetcode/problem912.go
+++ leetcode/problem912.go
@@ -78,3 +78,38 @@ func sortArray2(nums []int) []int {
 
 	return nums
 }
+
+//merge
+func merge(left, right []int) []int {
+
+	res := make([]int, 0, len(left)+len(right))
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) == 0 {
+			return append(res, right...)
+		}
+		if len(right) == 0 {
+			return append(res, left...)
+		}
+
+		if left[0] < right[0] {
+			res = append(res, left[0])
+			left = left[1:]
+		} else {
+			res = append(res, right[0])
+			right = right[1:]
+		}
+	}
+	return res
+}
+
+func sortArray3(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	middle := len(nums) / 2
+	left := sortArray(nums[:middle])
+	right := sortArray(nums[middle:])
+
+	return merge(left, right)
+}
