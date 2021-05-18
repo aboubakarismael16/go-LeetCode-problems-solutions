e4d290d946c6abafd3ee6e13c56699893b5de6ad
aboubakar ismael <aboubakarismael16@gmail.com>
2021-05-18 22:39:31 +0800

    second commit

--- LeetCode_Binary_Tree/problem105.go
+++ LeetCode_Binary_Tree/problem105.go
@@ -48,3 +48,24 @@ func buildTree2(preorder []int, inorder []int) *TreeNode {
 		Right: buildTree(preorder[i+1:], inorder[i+1:]),
 	}
 }
+
+//recursion
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	rootVal := preorder[0]
+	root := &TreeNode{
+		Val: rootVal,
+	}
+
+	var i int
+	for i = 0; i < len(inorder); i++ {
+		if inorder[i] == rootVal {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:i+1], inorder[:i])
+	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+	return root
+}
